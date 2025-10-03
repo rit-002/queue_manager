c6b9baba68288f0f850ab0af8afa705df19207c4
Ritesh <81522483+rit-002@users.noreply.github.com>
2025-10-04 01:14:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const QueueManager = require("./src/queue.js");
-module.exports = QueueManager;
+const QueueManager = require("./src/queue");
+const queue = new QueueManager();
+
+(async () => {
+  await queue.createQueue("myEvent", "org1", 2, "Test Event");
+  console.log(await queue.joinQueue("myEvent", "org1", "user1"));
+  console.log(await queue.joinQueue("myEvent", "org1", "user2"));
+  console.log(await queue.joinQueue("myEvent", "org1", "user3"));
+  console.log(await queue.getQueueStatus("myEvent", "org1"));
+})();
