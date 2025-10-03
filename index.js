c42e2b25c7127223151a4d7f6f19d4a98728c0f5
Ritesh <81522483+rit-002@users.noreply.github.com>
2025-10-04 01:32:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const QueueManager = require("./src/queue");
+// queueInstance.js
+const QueueManager = require("./src/queue"); // your QueueManager path
 const queue = new QueueManager();
-
-(async () => {
-  await queue.createQueue("myEvent", "org1", 2, "Test Event");
-  console.log(await queue.joinQueue("myEvent", "org1", "user1"));
-  console.log(await queue.joinQueue("myEvent", "org1", "user2"));
-  console.log(await queue.joinQueue("myEvent", "org1", "user3"));
-  console.log(await queue.getQueueStatus("myEvent", "org1"));
-})();
+module.exports = queue;
